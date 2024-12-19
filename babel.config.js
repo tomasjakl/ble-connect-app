25e154422c1fe81524c3abf2f44284ed17b94c7e
Tomas Jakl <tomas.jakl2@gmail.com>
2024-12-19 04:42:56 +0100

    build: upgrade RN version

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          'react-native-vector-icons/MaterialCommunityIcons':
+            '@react-native-vector-icons/material-design-icons',
+        },
+      },
+    ],
+  ],
 };
